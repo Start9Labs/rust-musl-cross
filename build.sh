18f0efa60e86cb6f002acb98f8a24930f452529f
Aiden McClelland <me@drbonez.dev>
2021-12-21 18:13:08 -0700

    build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo 'Building Container'
+docker build . -t start9/rust-musl-cross:aarch64-musl
+echo << EOF | sudo tee /usr/local/bin/rust-aarch64-musl-builder
+#!/bin/bash
+docker run --rm -it -v "$HOME/.cargo/registry":/root/.cargo/registry -v $(pwd):/home/rust/src start9/rust-musl-cross:aarch64-musl $@
+EOF
+sudo chmod +x /usr/local/bin/rust-aarch64-musl-builder
