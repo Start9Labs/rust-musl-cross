241ae7256838972a7157ec28a92f3490c7dd2708
Aiden McClelland <me@drbonez.dev>
2023-02-20 13:03:25 -0700

    fix script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 echo 'Building Container'
 docker build . -t start9/rust-musl-cross:aarch64-musl
-echo << EOF | sudo tee /usr/local/bin/rust-aarch64-musl-builder
+cat << 'EOF' | sudo tee /usr/local/bin/rust-aarch64-musl-builder
 #!/bin/bash
 docker run --rm -it -v "$HOME/.cargo/registry":/root/.cargo/registry -v $(pwd):/home/rust/src start9/rust-musl-cross:aarch64-musl $@
 EOF
